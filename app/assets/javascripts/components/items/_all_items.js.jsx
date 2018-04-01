bbacf2af5a204849eeab00b0f7d2fa7fa81c3880
Matt Holloway <hollowmatt@gmail.com>
2018-04-01 17:47:31 -0400

    built out items component to render items from db

--- app/assets/javascripts/components/items/_all_items.js.jsx
+++ app/assets/javascripts/components/items/_all_items.js.jsx
@@ -1,8 +1,28 @@
 class AllItems extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state =  {
+			items: [] 
+		};
+	}
+
+	componentDidMount() {
+		$.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) })
+	}
+
 	render() {
+		var items = this.state.items.map((item) => {
+			return(
+				<div key={item.id}>
+					<h3>{item.name}</h3>
+					<p>{item.description}</p>
+				</div>
+			)
+		});
+
 		return (
 			<div>
-				<p> item </p>
+				{items}
 			</div>
 		);
 	}

c8669684a7d1ffb87af6f44c676aec52af0bfa69
Matt Holloway <hollowmatt@gmail.com>
2018-04-01 18:06:45 -0400

    Working on form to add new item

--- app/assets/javascripts/components/items/_new_item.js.jsx
+++ app/assets/javascripts/components/items/_new_item.js.jsx
@@ -1,9 +1,41 @@
 class NewItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state =  {
+			name: '',
+			description: '' 
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({name: event.target.name, description: event.target.description});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		$.ajax({
+			url: '/api/v1/items',
+			type: 'POST',
+			data: { item: { name: this.state.name, description: this.state.description } },
+			success: (response) => {
+				console.log('it worked', response);
+			}
+		});
+	}
+
 	render() {
 		return(
-			<div>
-				<p> New Item </p>
-			</div>
+			<form on Submit={this.handleSubmit}>
+				<label>Name:
+					<input type="text" value={this.state.name} placeholder='Enter the name of the item' onChange={this.handleChange} />
+				</label>
+				<label>description:
+					<input type="text" value={this.state.description} placeholder='Enter a description' onChange={this.handleChange}/>
+				</label>
+				<input type="submit" value="Add Item" />
+			</form>
 		);
 	}
 }

701d8485d9f0b15f2371646c09d80d19e21aeecd
Matt Holloway <hollowmatt@gmail.com>
2018-04-02 21:03:05 -0400

    Finally got form fixed

--- app/assets/javascripts/components/items/_new_item.js.jsx
+++ app/assets/javascripts/components/items/_new_item.js.jsx
@@ -3,31 +3,46 @@ class NewItem extends React.Component {
 		super(props);
 		this.state =  {
 			inputName: '',
-			intputDescription: '' 
+			inputDescription: '' 
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	handleChange(event) {
-		console.log("Event: " + event);
-		this.setState({name: event.target.inputName, description: event.target.inputDescription});
+		const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
 	}
 
 	handleSubmit(event) {
-		alert('Content... name: ' + this.state.name + ", description: " + this.state.description);
-		event.preventDefault();
-
+		alert('Content... name: ' + this.state.inputName + ", description: " + this.state.inputDescription);
+		$.ajax({ 
+			url: '/api/v1/items', 
+			type: 'POST', 
+			data: { 
+				item: { 
+					name: this.state.inputName, 
+					description: this.state.inputDescription 
+				} 
+			}, 
+			success: (response) => { 
+				console.log('it worked!', response); 
+			} 
+		});
 	}
 
 	render() {
 		return(
 			<form onSubmit={this.handleSubmit}>
 				<label>Name:
-					<input type="text" value={this.state.inputName} onChange={this.handleChange}/>
+					<input type="text" name="inputName" value={this.state.inputName} onChange={this.handleChange}/>
 				</label>
 				<label>description:
-					<input type="text" value={this.state.inputDescription} onChange={this.handleChange}/>
+					<input type="text" name="inputDescription" value={this.state.inputDescription} onChange={this.handleChange}/>
 				</label>
 				<input type="submit" value="Add Item" />
 			</form>
